d871dfa5fb9c38c23943b9a8aca1f4afa2e60487
oalt <oa@lacheweg21.de>
2025-04-02 15:51:55 +0200

    Just a git demo

--- src/KK.BusinessCards.Apps.BlazorServer/KK.BusinessCards.Apps.BlazorServer/Program.cs
+++ src/KK.BusinessCards.Apps.BlazorServer/KK.BusinessCards.Apps.BlazorServer/Program.cs
@@ -4,7 +4,7 @@ using KK.BusinessCards.Apps.BlazorServer.ViewModels;
 namespace KK.BusinessCards.Apps.BlazorServer
 {
     public class Program
-    {
+    { 
         public static void Main(string[] arguments)
         {
       
